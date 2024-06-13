51582a72debb4bd00536b48e080460e5b50ff816
darcube <daria.sobieraj@gmail.com>
2024-06-13 23:10:23 +0200

    authorization with clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,30 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+
+export default clerkMiddleware((auth, request) => {
+    if(auth().userId && isPublicRoute(request)) {
+      let path = "/select-org"
+
+      if(auth().orgId) {
+        path = `/organization/${auth().orgId}`
+      }
+
+      const orgSelection = new URL(path, request.url)
+      return NextResponse.redirect(orgSelection)
+    }    
+
+     if (!auth().userId && !isPublicRoute(request)) {
+       return auth().redirectToSignIn()
+     }
+
+    if (auth().userId && !auth().orgId && request.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL('/select-org', request.url)
+      return NextResponse.redirect(orgSelection)
+    }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],

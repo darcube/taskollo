d835de33769f49b30d9b60214291cba8b9f829aa
darcube <daria.sobieraj@gmail.com>
2024-07-07 22:03:31 +0200

    settings and board server actions

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,34 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/organization(.*)',
+  '/board(.*)',
+  '/select-org',
+  '/api/cards(.*)'
+])
 
-export default clerkMiddleware((auth, request) => {
-    if(auth().userId && isPublicRoute(request)) {
-      let path = "/select-org"
 
-      if(auth().orgId) {
-        path = `/organization/${auth().orgId}`
-      }
 
-      const orgSelection = new URL(path, request.url)
-      return NextResponse.redirect(orgSelection)
-    }    
+export default clerkMiddleware((auth,req,event)=>{
+  if(!auth().userId && isProtectedRoute(req)){
+    return auth().redirectToSignIn({returnBackUrl:req.url})
+  } 
+  
+  if(auth().userId && !isProtectedRoute(req)){
+    let path : string = '/select-org'
 
-     if (!auth().userId && !isPublicRoute(request)) {
-       return auth().redirectToSignIn()
-     }
-
-    if (auth().userId && !auth().orgId && request.nextUrl.pathname !== "/select-org") {
-      const orgSelection = new URL('/select-org', request.url)
-      return NextResponse.redirect(orgSelection)
+    if( auth().orgId){
+      path = `/organization/${auth().orgId}`
     }
+    return NextResponse.redirect(new URL(path, req.url))
+  }
+
+  if(auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org" ){
+    return NextResponse.redirect(new URL('/select-org', req.url))
+  }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
